5e1c39022a623734c8de7e32964b6c9b563d8f44
Mohammed Ali <moh_fcis@yahoo.com>
2015-11-18 06:20:16 +0200

    Pass the date from js to template

--- Image-Share.js
+++ Image-Share.js
@@ -1,23 +1,11 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    var image_date = {
+        img_src: "bass.jpg",
+        img_alt:"base"
+    };
+    Template.images.helpers(image_date);
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
 }
