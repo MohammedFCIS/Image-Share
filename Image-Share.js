664d56cc81b85dcbee0f19d2b18b148dab34822d
Mohammed Ali <moh_fcis@yahoo.com>
2015-11-18 06:27:47 +0200

    convert single object to array of objects

--- Image-Share.js
+++ Image-Share.js
@@ -1,9 +1,19 @@
 if (Meteor.isClient) {
-    var image_date = {
+    var image_date = [
+        {
         img_src: "bass.jpg",
         img_alt:"base"
-    };
-    Template.images.helpers(image_date);
+        },
+        {
+        img_src: "beard.jpg",
+        img_alt:"beard.jpg"
+        },
+        {
+        img_src: "laptops.jpg",
+        img_alt:"laptops.jpg"
+        }
+    ];
+    Template.images.helpers({images:image_date});
 }
 
 if (Meteor.isServer) {
