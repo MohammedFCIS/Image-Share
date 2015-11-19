7f420f7cb7a66460154b2ee4b6a686a565ce5764
Mohammed Ali <moh_fcis@yahoo.com>
2015-11-19 06:08:54 +0200

    remove old images array

--- Image-Share.js
+++ Image-Share.js
@@ -2,27 +2,13 @@ var images = new Mongo.Collection("images");
 
 
 if (Meteor.isClient) {
-    var image_date = [
-        {
-        img_src: "bass.jpg",
-        img_alt:"base"
-        },
-        {
-        img_src: "beard.jpg",
-        img_alt:"beard.jpg"
-        },
-        {
-        img_src: "laptops.jpg",
-        img_alt:"laptops.jpg"
-        }
-    ];
     Template.images.helpers({images:images.find()});
     Template.images.events({
       "click .js-img": function(event, template){
         $(event.target).css("width","50%");
       },
       "click .js-del-btn":function(event, template){
-          
+
         }
 
     });
