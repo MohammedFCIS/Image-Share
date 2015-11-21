332595767823bcd341b289cb069582cc20dfdb06
Mohammed Ali <moh_fcis@yahoo.com>
2015-11-21 15:16:36 +0200

    Finalize routing

--- client/main.js
+++ client/main.js
@@ -1,5 +1,36 @@
 
+/// routing
 
+Router.configure({
+  layoutTemplate: 'ApplicationLayout'
+});
+
+Router.route('/', function () {
+  this.render('welcome', {
+    to:"main"
+  });
+});
+
+Router.route('/images', function () {
+  this.render('navbar', {
+    to:"navbar"
+  });
+  this.render('images', {
+    to:"main"
+  });
+});
+
+Router.route('/image/:_id', function () {
+  this.render('navbar', {
+    to:"navbar"
+  });
+  this.render('image', {
+    to:"main",
+    data:function(){
+      return images.findOne({_id:this.params._id});
+    }
+  });
+});
 
 /// infiniscroll
 
