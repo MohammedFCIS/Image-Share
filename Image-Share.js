f8e20faefe6590e78ff3b24c3aeb6b27ba89bac5
Mohammed Ali <moh_fcis@yahoo.com>
2015-11-21 12:50:47 +0200

    Finish the infinite scroll feature

--- Image-Share.js
+++ Image-Share.js
@@ -4,9 +4,18 @@ var images = new Mongo.Collection("images");
 if (Meteor.isClient) {
   Session.set('imageLimit', 8);
   lastScrollTop = 0;
-  $(window).scroll(function(events){
-    if($(window).scrollTop() + $(window).height() >= $(document).height()){
-      console.log(new Date());
+  $(window).scroll(function(event){
+    // test if we are near the bottom of the window
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+      // where are we in the page?
+      var scrollTop = $(this).scrollTop();
+      // test if we are going down
+      if (scrollTop > lastScrollTop){
+        // yes we are heading down...
+       Session.set("imageLimit", Session.get("imageLimit") + 4);
+      }
+
+      lastScrollTop = scrollTop;
     }
 
   });
